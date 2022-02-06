c4c032b1ac670fd1b8125dca5b9d9a0b2f8c07f2
juliano-soares <37117169+juliano-soares@users.noreply.github.com>
2022-02-05 23:10:50 -0300

    Finalizada a terceira aula

--- meulink/src/components/LinkItem/index.js
+++ meulink/src/components/LinkItem/index.js
@@ -1,7 +1,11 @@
 import './link-item.css';
 import {FiX, FiClipboard} from 'react-icons/fi'
 
-export default function LinkItem({closeModal}) {
+export default function LinkItem({closeModal, content}) {
+    async function copyLink() {
+        await navigator.clipboard.writeText(content.link);
+    }
+
     return (
         <div className="modal-container">
             <div className="modal-header">
@@ -12,11 +16,11 @@ export default function LinkItem({closeModal}) {
             </div>
 
             <span>
-                https://www.google.com
+                {content.long_url}
             </span>
 
-            <button className="modal-link">
-                https://nit.ly/12900
+            <button className="modal-link" onClick={copyLink}>
+                {content.link}
                 <FiClipboard size={20} color="#FFF" />
             </button>
         </div>
--- meulink/src/pages/Home/index.js
+++ meulink/src/pages/Home/index.js
@@ -5,12 +5,26 @@ import './home.css';
 import Menu from '../../components/Menu';
 import LinkItem from '../../components/LinkItem';
 
+import api from '../../services/api';
+
 export default function Home() {
   const [link, setLink] = useState('');
+  const [data, setData] = useState({});
   const [showModal, setShowModal] = useState(false);
   
-  function handleShortLink() {
-    setShowModal(true);
+  async function handleShortLink() {
+    try {
+      const response = await api.post('/shorten', { 
+        long_url: link,
+      })
+
+      setData(response.data);
+      setShowModal(true);
+
+      setLink('');
+    } catch (error) {
+      setLink('');
+    }
   }
 
   return (
@@ -35,7 +49,10 @@ export default function Home() {
 
       <Menu />
       {showModal && (
-        <LinkItem closeModal={() => setShowModal(false)}/>
+        <LinkItem 
+          closeModal={() => setShowModal(false)}
+          content={data}
+        />
       )}
     </div>
   );
