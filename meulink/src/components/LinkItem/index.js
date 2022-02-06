67e82ab17208d6f37d69924da9e7b9bc9f98e4c8
juliano-soares <37117169+juliano-soares@users.noreply.github.com>
2022-02-05 21:30:41 -0300

    Adiciona modal e página 404

---
+++ meulink/src/components/LinkItem/index.js
@@ -0,0 +1,24 @@
+import './link-item.css';
+import {FiX, FiClipboard} from 'react-icons/fi'
+
+export default function LinkItem({closeModal}) {
+    return (
+        <div className="modal-container">
+            <div className="modal-header">
+                <h2>Link Encurtado</h2>
+                <button onClick={closeModal}>
+                    <FiX size={28} color="#000" />
+                </button>
+            </div>
+
+            <span>
+                https://www.google.com
+            </span>
+
+            <button className="modal-link">
+                https://nit.ly/12900
+                <FiClipboard size={20} color="#FFF" />
+            </button>
+        </div>
+    )
+}
--- meulink/src/pages/Home/index.js
+++ meulink/src/pages/Home/index.js
@@ -1,29 +1,43 @@
-import {FiLink} from 'react-icons/fi'
+import {useState} from 'react';
+import {FiLink} from 'react-icons/fi';
 import './home.css';
 
 import Menu from '../../components/Menu';
+import LinkItem from '../../components/LinkItem';
 
 export default function Home() {
-    return (
-      <div className="container-home">
-        <div className="logo">
-          <img src="/logo.png" alt="Sujeito Link Logo" />
-          <h1>Sujeito Link</h1>
-          <span>Cole seu link para encurtar 👇</span>
-        </div>
-
-        <div className="area-input">
-          <div>
-            <FiLink size={24} color="#fff" />
-            <input 
-              placeholder="Cole seu link aqui..."
-            />
-          </div>
-          <button>Encurtar Link</button>
-        </div>
-
-        <Menu />
-      </div>
-    );
+  const [link, setLink] = useState('');
+  const [showModal, setShowModal] = useState(false);
+  
+  function handleShortLink() {
+    setShowModal(true);
   }
+
+  return (
+    <div className="container-home">
+      <div className="logo">
+        <img src="/logo.png" alt="Sujeito Link Logo" />
+        <h1>Sujeito Link</h1>
+        <span>Cole seu link para encurtar 👇</span>
+      </div>
+
+      <div className="area-input">
+        <div>
+          <FiLink size={24} color="#fff" />
+          <input 
+            placeholder="Cole seu link aqui..."
+            value={link.id}
+            onChange={(e) => setLink(e.target.value)}
+          />
+        </div>
+        <button onClick={handleShortLink}>Encurtar Link</button>
+      </div>
+
+      <Menu />
+      {showModal && (
+        <LinkItem closeModal={() => setShowModal(false)}/>
+      )}
+    </div>
+  );
+}
   
